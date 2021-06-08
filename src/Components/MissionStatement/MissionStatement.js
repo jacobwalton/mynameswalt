0f81c33b86fb981053cc31fd9c5b14fde22c3cc5
Jacob Walton <jawalton6616@gmail.com>
2021-06-08 11:00:26 -0500

    removed animations for better performance

--- src/Components/MissionStatement/MissionStatement.js
+++ src/Components/MissionStatement/MissionStatement.js
@@ -8,12 +8,15 @@ const MissionStatement = () => {
         <h2>My Mission Statement as a Creator</h2>
 
         <p id="missionStatementText">
+          <hr color="grey" />
           My goal has remained entact since I first started creating; to change
-          the world. While working towards this goal, I hope to serve as a
-          catalyst to bring forth healing, growth, expression and love in
-          individuals. For those not ready or interested in change, at the very
-          least, I strive to take individuals to another world with my
-          creations.
+          the world.
+          <br />
+          While working towards this goal, I hope to serve as a catalyst to
+          bring forth healing, growth, expression and love in individuals.
+          <br />
+          For those not ready or interested in change, at the very least, I
+          strive to take individuals to another world with my creations.
         </p>
       </div>
     </div>
